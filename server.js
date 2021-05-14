665959d40b21e069dda38bafa658c0a4a1b28232
Hoang Duc Anh Nguyen <anhnguyen3120@gmail.com>
2021-05-14 18:42:57 +0700

    Config for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 // Use process.env.PORT for Heroku deployment.
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
